3980896039f542c1d2ce52439f13a5add648c618
limingfeng <limingfeng@didiglobal.com>
2019-08-23 17:48:37 +0800

    更新用户登录模块

--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,4 @@
-"""tang URL Configuration
+"""mainsite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -13,14 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
-from django.urls import path, include
 
-from tang import views
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^$', views.hello),
-    path('user/', include('user.urls')),
+    path('login/', views.login),
+    path('result/', views.login_result),
 ]
+
--- user/views.py
+++ user/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+
+
+def login(request):
+    context = {}
+    context['hello'] = 'Hello World!'
+    context['title'] = 'Login'
+    return render(request, 'login.html', context)
+
+
+@csrf_exempt
+def login_result(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+    print("username:"+username+",password:"+password)
+    # return HttpResponse("Hello world ! "  + ":" )
+
+    # user = User.objects.filter(username=username, password=my_md5(password))
+    # if user:
+    #     # 将用户的username保存到session中
+    #     request.session["login_user"] = username
+
+    context = {}
+    context['content'] = '登录成功'
+    context['base_title'] = 'Login Result'
+    return render(request, 'login_result.html', context)
