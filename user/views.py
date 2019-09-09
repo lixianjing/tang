435e6c2c92b6abb904754f22a0ddadb19f27167a
limingfeng <limingfeng@didiglobal.com>
2019-09-09 18:17:36 +0800

    更新显示页面

--- user/views.py
+++ user/views.py
@@ -15,12 +15,14 @@ def login(request):
 
 @csrf_exempt
 def login_result(request):
-
+    b = False
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+        if username == 'lmf':
+            b = True
 
-    print("username:"+username+",password:"+password)
+    print("username:" + username + ",password:" + password)
     # return HttpResponse("Hello world ! "  + ":" )
 
     # user = User.objects.filter(username=username, password=my_md5(password))
@@ -28,7 +30,10 @@ def login_result(request):
     #     # 将用户的username保存到session中
     #     request.session["login_user"] = username
 
-    context = {}
-    context['content'] = '登录成功'
-    context['base_title'] = 'Login Result'
-    return render(request, 'login_result.html', context)
+    if b:
+        context = {}
+        context['content'] = '登录成功'
+        context['base_title'] = 'Login Result'
+        return render(request, 'login_result.html', context)
+    else:
+        return redirect(reverse("user:login"))
